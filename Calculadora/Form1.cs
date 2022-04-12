0d02e5cb3318981ebf5f7021b0e310f1a7298a20
Andres Felipe Velez Alcaraz <andres.velez5136@uco.net.co>
2022-04-11 19:48:44 -0500

    Escribi en el archivo

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Calculadora
+namespace Calculadora //Coloque un hola
 {
     public partial class Form1 : Form
     {
