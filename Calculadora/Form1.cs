79ddf93e5908ddcfeb96e8e53654598b9fd3ec2a
Andres Felipe Velez Alcaraz <andres.velez5136@uco.net.co>
2022-04-11 20:08:52 -0500

    Comentario para subir con push

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Calculadora //Coloque un hola
+namespace Calculadora //ESte es otro comentario para hacer bien la practica
 {
     public partial class Form1 : Form
     {
